c6eb726fb7adfabf95d45d28e21c10ef9a53e186
Nerldy_ <arnodhis@gmail.com>
2018-03-24 08:35:24 +0300

    add TODO comments

---
+++ routes.py
@@ -0,0 +1,13 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+# TODO: POST /api/books - Add a book
+# TODO: PUT /api/books/<bookId> - Remove a book
+# TODO: GET /api/books - Retrieves all books
+# TODO: GET /api/books/<bookId> - Get a book
+# TODO: POST /api/users/books/<bookId> - Borrow a book
+# TODO: POST /api/auth/register - Creates a user account
+# TODO: POST /api/auth/login - Logs in user
+# TODO: POST /api/auth/logout - Logs out user
+# TODO: POST /api/auth/reset-password - Password reset
